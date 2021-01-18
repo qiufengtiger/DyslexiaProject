15ebb7cd7222a957bd333695bc6c7ef2bc63ca85
qiufengtiger <zfengr@me.com>
2021-01-18 00:19:58 -0500

    done convert

--- NN/ConvertBinary.py
+++ NN/ConvertBinary.py
@@ -2,15 +2,48 @@
 # Everything but gender_dummy and teacher are converted to numbers for NN input
 
 import csv
+entryName = []
+fileData = []
 
 def parseFile(inputPath):
-	with open(inputPath) as inputFile:
+	global fileData, entryName
+	with open(inputPath, 'r') as inputFile:
 		reader = csv.reader(inputFile, delimiter = ",", quotechar = '"')
 		fileData = [row for row in reader]
-	print(fileData[0])
+	# retrieve column names
+	entryName = fileData.pop(0)
+		
+def convert():
+	global filaData
+	for row in fileData:
+		# known result
+		group = row[16]
+		# remove not used data
+		row.pop(7) # school
+		row.pop(6) # teacher
+		row.pop(3) # dummy gender
+		row.pop(1) # DOB
+		row.pop(0) # name
 
+		
+		
+		
+		
+
+		# move known result to the end
+		row.pop(16)
+		row.append(group)
+		
+def writeResult(outputPath):
+	with open(outputPath, 'w+') as outputFile:
+		writer = csv.writer(outputFile, delimiter = ",")
+		writer.writerows(fileData)
 
 
 if __name__ == '__main__':
 	# filename = sys.argv[1]
-	filename = "../overallData.csv"
+	inputFile = "../overallData.csv"
+	outputFile = "../overallDataConverted.csv"
+	parseFile(inputFile)
+	convert()
+	writeResult(outputFile)
